28c5d9acb39a20f0028f2712e33d7cb5570f6e2c
Cesar Carruitero <cesar@mozilla.pe>
2017-10-20 23:30:56 -0500

    move extension generation to template file

--- lib/solidus_cmd/extension.rb
+++ lib/solidus_cmd/extension.rb
@@ -8,29 +8,16 @@ module SolidusCmd
     desc "builds a solidus extension"
     argument :file_name, type: :string, desc: 'rails app_path', default: '.'
 
-    source_root File.expand_path('../templates/extension', __FILE__)
+    def self.source_root
+      File.expand_path('../', __FILE__)
+    end
 
     def generate
       use_prefix 'solidus_'
 
       empty_directory file_name
 
-      directory 'app', "#{file_name}/app"
-      directory 'lib', "#{file_name}/lib"
-      directory 'bin', "#{file_name}/bin"
-
-      template 'extension.gemspec', "#{file_name}/#{file_name}.gemspec"
-      template 'Gemfile', "#{file_name}/Gemfile"
-      template 'gitignore', "#{file_name}/.gitignore"
-      template 'LICENSE', "#{file_name}/LICENSE"
-      template 'Rakefile', "#{file_name}/Rakefile"
-      template 'README.md', "#{file_name}/README.md"
-      template 'config/routes.rb', "#{file_name}/config/routes.rb"
-      template 'config/locales/en.yml', "#{file_name}/config/locales/en.yml"
-      template 'rspec', "#{file_name}/.rspec"
-      template 'spec/spec_helper.rb.tt', "#{file_name}/spec/spec_helper.rb"
-      template 'rubocop.yml', "#{file_name}/.rubocop.yml"
-      template 'travis.yml', "#{file_name}/.travis.yml"
+      apply 'template.rb'
     end
 
     no_tasks do
